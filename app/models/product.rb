c9a492722bc84610fae7b35fab82b4d7e962b692
daiyuqiang <250936666@qq.com>
2011-07-19 10:04:10 +0800

    345

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,7 +16,7 @@ class Product < ActiveRecord::Base
    
    def ensure_not_referenced_by_any_line_item
      
-     if lin_items.count.zero?
+     if line_items.count.zero?
        return true
      else
        errors[:base] << "Line Items present"
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -33,18 +33,25 @@ class ProductTest < ActiveSupport::TestCase
     assert product.valid?
   end
   
+  def new_product(image_url)
+      Product.new(        :title       =>"My Book Title",
+                          :description => "yyy",
+                          :price       => 1,
+                          :image_url   => image_url)
+  end
+  
   test "image url" do
-    ok= %w{fred.gift fres.jpg fred.png FRED.JPG FRED.Jpg
+    ok= %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
            http://a.b.c/x/y/z/fred.gif}
-    bad= %w{ fred.doc fred.gif/more fred.gif.more}
+    bad= %w{fred.doc fred.gif/more fred.gif.more}
     
     ok.each do |name|
-      assert new_product(name).valid?,"#{name},shouldn't be invalid"
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
     end
     
     bad.each do |name|
-      assert new_product(name).invalid?,"#{name},shouldn't be valid"
-    end
+      assert new_product(name).invalid?, "#{name},shouldn't be valid"
+    end 
     
   end
 end
