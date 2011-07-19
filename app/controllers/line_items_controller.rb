a5dec98c772d9569c161073502976656f86241f3
daiyuqiang <250936666@qq.com>
2011-07-19 09:21:29 +0800

    aaa

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,10 +46,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(store_url)}
-        format.js  { @current_item = @line_item }
-        format.xml  { render :xml => @line_item, 
-          :status => :created, :location => @line_item }
+        format.html { redirect_to(@line_item.cart)}
+        format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
