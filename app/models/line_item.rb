4a5283908d6710b51cb6fb834c10047829e0f8df
daiyuqiang <250936666@qq.com>
2011-07-19 09:46:00 +0800

    123

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,4 @@
 class LineItem < ActiveRecord::Base
-  belongs_to :order
   belongs_to :product
   belongs_to :cart
   
--- script/load_orders.rb
+++
@@ -1,6 +0,0 @@
-Order.transaction do
-  (1..100).each do |i|
-    Order.create(:name => "Customer #{i}", :address => "#{i} Main street",
-      :email => "customer-#{i}@example.com", :pay_type => "Check")
-  end
-end
